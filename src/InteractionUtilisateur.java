40aa451ad0020f6bae78a8d44f62752188cebb26
romain.chica <romain.chica@le-campus-numerique.fr>
2024-11-20 16:21:17 +0100

    refacto Main and InteractionUtilisateur

--- src/InteractionUtilisateur.java
+++ src/InteractionUtilisateur.java
@@ -1,4 +1,6 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
 
 public class InteractionUtilisateur {
     private Scanner scanner;
@@ -7,19 +9,17 @@ public class InteractionUtilisateur {
         scanner = new Scanner(System.in);
     }
 
-
-    // Affiche une question et récupère la répone sous forme d'entier
-    public int askForInt(String question, int min, int max ) {
+    public int askForInt(String question, int min, int max) {
         int value = -1;
         while (true) {
             try {
                 System.out.println(question + " ");
                 value = scanner.nextInt();
                 if (value >= min && value <= max) {
-                    break; // Sort si la valeur est valide
+                    break;
                 }
                 System.out.println("Veuillez entrer un nombre entre " + min + " et " + max + ".");
-            } catch (java.util.InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Entrée invalide. Veuillez entrer un nombre entier.");
                 scanner.next(); // Ignore l'entrée invalide
             }
@@ -27,7 +27,38 @@ public class InteractionUtilisateur {
         return value;
     }
 
-    // Ferme le scanner à la fin de l'utilisation
+    public Player[] choosePlayers() {
+        System.out.println("Choisissez la configuration des joueurs :");
+        System.out.println("1. Humain contre Humain");
+        System.out.println("2. Humain contre IA");
+        System.out.println("3. IA contre IA");
+
+        int choice = askForInt("Votre choix :", 1, 3);
+
+        Player playerX, playerO;
+
+        switch (choice) {
+            case 1:
+                playerX = new HumanPlayer(" X ", this);
+                playerO = new HumanPlayer(" O ", this);
+                break;
+            case 2:
+                playerX = new HumanPlayer(" X ", this);
+                playerO = new ArtificialPlayer(" O ");
+                break;
+            case 3:
+                playerX = new ArtificialPlayer(" X ");
+                playerO = new ArtificialPlayer(" O ");
+                break;
+            default:
+                System.out.println("Choix invalide, par défaut Humain contre IA.");
+                playerX = new HumanPlayer(" X ", this);
+                playerO = new ArtificialPlayer(" O ");
+        }
+
+        return new Player[]{playerX, playerO};
+    }
+
     public void close() {
         scanner.close();
     }
--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,12 @@ public class Main {
     public static void main(String[] args) {
         InteractionUtilisateur interactionUtilisateur = new InteractionUtilisateur();
         View view = new View();
-        Player playerX, playerO;
 
-        view.displayMessage("Choisissez la configuration des joueurs :");
-        view.displayMessage("1. Humain contre Humain");
-        view.displayMessage("2. Humain contre IA");
-        view.displayMessage("3. IA contre IA");
+        // Choix des joueurs via InteractionUtilisateur
+        Player[] players = interactionUtilisateur.choosePlayers();
 
-        int choice = interactionUtilisateur.askForInt("Votre choix :", 1, 3);
-
-        switch (choice) {
-            case 1:
-                playerX = new HumanPlayer(" X ", interactionUtilisateur);
-                playerO = new HumanPlayer(" O ", interactionUtilisateur);
-                break;
-            case 2:
-                playerX = new HumanPlayer(" X ", interactionUtilisateur);
-                playerO = new ArtificialPlayer(" O ");
-                break;
-            case 3:
-                playerX = new ArtificialPlayer(" X ");
-                playerO = new ArtificialPlayer(" O ");
-                break;
-            default:
-                view.displayMessage("Choix invalide, par défaut Humain contre IA.");
-                playerX = new HumanPlayer(" X ", interactionUtilisateur);
-                playerO = new ArtificialPlayer(" O ");
-        }
-
-        TicTacToe ticTacToe = new TicTacToe(playerX, playerO, view);
+        // Lancer le jeu avec les joueurs sélectionnés
+        TicTacToe ticTacToe = new TicTacToe(players[0], players[1], view);
         ticTacToe.play();
 
         interactionUtilisateur.close(); // Libère les ressources
